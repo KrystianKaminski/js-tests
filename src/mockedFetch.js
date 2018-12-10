fa3bb25e951410f3031380d8c584e90167280c78
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-10 14:05:46 +0100

    Mocked fetch assumptions

---
+++ src/mockedFetch.js
@@ -0,0 +1,16 @@
+// return promise
+// resolves to object that
+// has json property
+// is function that 
+// returns an array
+
+[
+    {
+        name: 'Lager',
+        price: 10
+    },
+    {
+        name: 'APA',
+        price: 15
+    },
+]
