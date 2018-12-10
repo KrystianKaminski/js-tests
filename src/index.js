9e66538a170028daff91793e9712e4d94e8eb6df
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-10 10:14:49 +0100

    Reduce method on orders array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const orders = [
+    {
+        name: 'Lager',
+        price: 10
+    },
+    {
+        name: 'APA',
+        price: 15
+    },
+]
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const orderTotal = (arrayOfOrders) => {
+    return arrayOfOrders.reduce(
+        (reduced, item, index, array) => {
+            reduced = item.price + reduced
+            return reduced
+        }, 0
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(orderTotal(orders)) // should log 25
