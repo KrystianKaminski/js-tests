31661ea4f363c93fd71e6eef2fe251236bd0405c
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-10 11:14:48 +0100

    Started using Jest

--- src/index.js
+++ src/index.js
@@ -1,33 +1,14 @@
-const orders = [
-    {
-        name: 'Lager',
-        price: 10
-    },
-    {
-        name: 'APA',
-        price: 15
-    },
-]
-
-const orderTotal = (arrayOfOrders) => (
+export const orderTotal = (arrayOfOrders) => (
 
     arrayOfOrders.reduce(
-        (reduced, item, index, array) => reduced += item.price
+        (reduced, item, index, array) => {
+            if (item.quantity) {
+                return (reduced += item.price
+                    * item.quantity)
+            } else {
+                return reduced += item.price
+            }
+        }
         ,0
     )
 )
-
-
-console.log(orderTotal(orders)) // should log 25
-
-if (
-    orderTotal(orders) !== 25
-) {
-    throw new Error('Happy path failed!')
-}
-
-if (
-    orderTotal([]) !== 0
-) {
-    throw new Error('Empty order list must equals 0')
-}
---
+++ src/index.test.js
@@ -0,0 +1,43 @@
+import { orderTotal } from './index'
+
+const orders = [
+    {
+        name: 'Lager',
+        price: 10
+    },
+    {
+        name: 'APA',
+        price: 15
+    },
+]
+
+test('Happy path failed!', () => {
+    expect(
+        orderTotal(orders)
+    ).toBe(25)
+})
+
+test('Empty order list must equals 0!', () => {
+    expect(
+        orderTotal([])
+    ).toBe(0)
+})
+
+const ordersWithQuantity = [
+    {
+        name: 'Lager',
+        price: 10,
+        quantity: 2,
+    },
+    {
+        name: 'APA',
+        price: 15,
+        quantity: 1
+    },
+]
+
+test('Fails to calculate sum with quantity', () => {
+    expect(
+        orderTotal(ordersWithQuantity)
+    ).toBe(35)
+})
