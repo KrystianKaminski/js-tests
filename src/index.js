d5dcf691dedae0430e465a5f45273a96da86d3a4
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-10 10:31:58 +0100

    Some refactoring

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const orders = [
     },
 ]
 
-const orderTotal = (arrayOfOrders) => {
-    return arrayOfOrders.reduce(
-        (reduced, item, index, array) => {
-            reduced = item.price + reduced
-            return reduced
-        }, 0
+const orderTotal = (arrayOfOrders) => (
+
+    arrayOfOrders.reduce(
+        (reduced, item, index, array) => reduced += item.price
+        ,0
     )
-}
+)
+
 
 console.log(orderTotal(orders)) // should log 25
 
