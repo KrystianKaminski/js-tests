8d8098e67d23b1ab6315cefca248a5c80c173475
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-10 11:50:34 +0100

    Describe function in jest

--- src/index.test.js
+++ src/index.test.js
@@ -1,53 +1,59 @@
 import { orderTotal } from './index'
 
-const orders = [
-    {
-        name: 'Lager',
-        price: 10
-    },
-    {
-        name: 'APA',
-        price: 15
-    },
-]
+describe('Sum without quantity', () => {
 
-test('Happy path failed!', () => {
-    expect(
-        orderTotal(orders)
-    ).toBe(25)
+    const orders = [
+        {
+            name: 'Lager',
+            price: 10
+        },
+        {
+            name: 'APA',
+            price: 15
+        },
+    ]
+    
+    test('Happy path failed!', () => {
+        expect(
+            orderTotal(orders)
+        ).toBe(25)
+    })
+    
+    test('Empty order list must equals 0!', () => {
+        expect(
+            orderTotal([])
+        ).toBe(0)
+    })
 })
 
-test('Empty order list must equals 0!', () => {
-    expect(
-        orderTotal([])
-    ).toBe(0)
-})
+describe('Sum WITH quantity', () => {
 
-const ordersWithQuantity = [
-    {
-        name: 'Lager',
-        price: 10,
-        quantity: 2,
-    },
-    {
-        name: 'APA',
-        price: 15,
-        quantity: 1
-    },
-]
-
-test('Fails to calculate sum with quantity', () => {
-    expect(
-        orderTotal(ordersWithQuantity)
-    ).toBe(35)
-})
-
-test('Fails to calculate sum with ZERO quantity', () => {
-    expect(
-        orderTotal([{
+    const ordersWithQuantity = [
+        {
             name: 'Lager',
             price: 10,
-            quantity: 0,
-        }])
-    ).toBe(0)
+            quantity: 2,
+        },
+        {
+            name: 'APA',
+            price: 15,
+            quantity: 1
+        },
+    ]
+    
+    test('Fails to calculate sum with quantity', () => {
+        expect(
+            orderTotal(ordersWithQuantity)
+        ).toBe(35)
+    })
+    
+    test('Fails to calculate sum with ZERO quantity', () => {
+        expect(
+            orderTotal([{
+                name: 'Lager',
+                price: 10,
+                quantity: 0,
+            }])
+        ).toBe(0)
+    })
 })
