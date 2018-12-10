99bef0c3d05069145c536a9fc3f9857ea352800a
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-10 10:25:42 +0100

    Test naming

--- src/index.js
+++ src/index.js
@@ -23,11 +23,11 @@ console.log(orderTotal(orders)) // should log 25
 if (
     orderTotal(orders) !== 25
 ) {
-    throw new Error('Simple sum failed!')
+    throw new Error('Happy path failed!')
 }
 
 if (
     orderTotal([]) !== 0
 ) {
-    throw new Error('Simple sum failed!')
+    throw new Error('Empty order list must equals 0')
 }
